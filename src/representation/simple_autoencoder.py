e6901ab6da0848e792ec3f7b5006231fcc66fe72
weidler <web@tonioweidler.de>
2018-11-05 18:55:30 +0100

    Autoencoder can learn index

--- src/representation/simple_autoencoder.py
+++ src/representation/simple_autoencoder.py
@@ -10,32 +10,34 @@ class Autoencoder(nn.Module):
         super(Autoencoder, self).__init__()
 
         self.encoder = nn.Linear(8, 3)
-        self.decoder = nn.Linear(3, 8)
+        self.decoder = nn.Linear(3, 9)
 
         self.activation = torch.sigmoid
 
     def forward(self, vinput):
         out = self.activation(self.encoder(vinput))
-        out = self.activation(self.decoder(out))
+        out = (self.decoder(out))
 
         return out
 
 
 if __name__ == "__main__":
     net = Autoencoder()
-    optimizer = optim.SGD(net.parameters(), lr=0.999)
+    optimizer = optim.SGD(net.parameters(), lr=1)
     criterion = nn.MSELoss()
 
     data = [torch.Tensor([0 if j != i else 1 for j in range(8)]).view(1, -1) for i in range(8)]
     print(data)
 
-    for epoch in range(10000):
+    for epoch in range(100000):
         for sample in data:
             optimizer.zero_grad()
             out = net(sample)
-            loss = criterion(out, sample)
+            a = (torch.argmax(sample, dim=1)).view(1, -1)
+            loss = criterion(out, torch.cat((sample.float(), torch.argmax(sample, dim=1).view(1, -1).float()), dim=1))
             loss.backward()
             optimizer.step()
+        if (epoch % 1000 == 0): print(epoch)
 
     for sample in data:
         print(sample)

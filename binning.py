d12e835de6e802214872aa4495d1ef21a4c82341
Kevin Trebing <Kevin.Trebing@gmx.net>
2018-11-06 10:20:38 +0100

    np.searchsorted looks promising

--- binning.py
+++ binning.py
@@ -13,6 +13,13 @@ def binThese(lowerBound, upperBound, bins=10, cutOffInf=10000):
     return featureBins[:, :-1]
 
 
+def getState(allStates, features):
+    bin = []
+    for f in features:
+        bin.append(np.searchsorted(allStates, f))
+        return allStates[bin]
+
+
 stateBins = binThese([-5, 0, 0.08], [5, np.inf, 0.2], bins=10, cutOffInf=5)
 allStates = cartesian(stateBins)
 
@@ -21,8 +28,14 @@ print(allStates[0, :])
 print(allStates[1, :])
 randomState = [-4.8, 0.7, 0.104]
 print(f'randomState: {randomState}')
+print(stateBins)
+print(allStates)
+a = np.searchsorted(stateBins[:, 0], randomState[0])
+b = np.searchsorted(stateBins[:, 1], randomState[1])
+c = np.searchsorted(stateBins[:, 2], randomState[2])
+print(a, b, c)
 # temp = allStates-randomState
 # temp = temp[temp[:, 0] >= 0]
 # temp = temp[temp[:, 1] >= 0]
 # temp = temp[temp[:, 2] >= 0]
-# print(f'bin: {temp[0]}')
+print(f'bin: {allStates[a,b,c]}')

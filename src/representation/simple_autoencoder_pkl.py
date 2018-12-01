dc2d0e564cefd2a8c23fbebc028123cf74a4c7b2
Unknown <liu.danni@hotmail.com>
2018-12-01 18:35:47 +0100

    Commented out normalization due to side effects
    Need to be scaled back to original feature range in when training RL agent

--- src/representation/simple_autoencoder_pkl.py
+++ src/representation/simple_autoencoder_pkl.py
@@ -14,18 +14,18 @@ if __name__ == "__main__":
 
     # normalize input to mean=0, variance=1
     # currently it's done not so efficiently to avoid changes in other parts of code
-    assert len(data) != 0
-    normalized_input = np.zeros((len(data), len(data[0][0])))  # shape is num_sinstances *  num_features
-
-    for i, instance in enumerate(data):
-        normalized_input[i] = np.array(instance[0])
-    m = np.mean(normalized_input, 0)
-    std = np.std(normalized_input, 0)
-    normalized_input = (normalized_input - m) / std
-
-    for i in np.arange(len(data)):
-        data[i][0] = normalized_input[i]
-    print('Finished normalization')
+    # assert len(data) != 0
+    # normalized_input = np.zeros((len(data), len(data[0][0])))  # shape is num_sinstances *  num_features
+    #
+    # for i, instance in enumerate(data):
+    #     normalized_input[i] = np.array(instance[0])
+    # m = np.mean(normalized_input, 0)
+    # std = np.std(normalized_input, 0)
+    # normalized_input = (normalized_input - m) / std
+    #
+    # for i in np.arange(len(data)):
+    #     data[i][0] = normalized_input[i]
+    # print('Finished normalization')
 
     net = Autoencoder()
     optimizer = optim.SGD(net.parameters(), lr=0.1)

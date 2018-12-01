6427e287faeac015845071975de70240f4e9b2ef
Kevin Trebing <Kevin.Trebing@gmx.net>
2018-12-01 16:32:45 +0100

    fixed autoencoder file reading, removed unused allStates variable in getState

--- src/representation/test_autoencoder.py
+++ src/representation/test_autoencoder.py
@@ -4,25 +4,26 @@ import random
 import torch
 from torch import nn
 
-from src.representation.autoencoder import Autoencoder
+from autoencoder import Autoencoder
 
-net = Autoencoder()
+net = Autoencoder(5, 3, 4)
 #net.load_state_dict(torch.load('../../models/hidden-2-ae.model'))
-net.load_state_dict(torch.load('../../models/very-simple.model'))
+net.load_state_dict(torch.load('../../models/sas.model'))
 
 criterion = nn.MSELoss()
 
-with open("../../data/cartpole.pkl", "rb") as f:
-    data = pickle.load(f)
+with open("../../data/cartpole.data") as f:
+    data = [list(map(eval, l[:-1].split("\t"))) for l in f.readlines()]
 
 errors = []
 
 for i in range(100):  # test 100 random inputs
-    random_index = random.randint(0, len(data)-1)
-    input_net = torch.tensor(data[random_index][0]).float()
+    # test predicting next state given current one + action
+    random_index = random.randint(0, len(data)-2)
+    input_net = torch.tensor(data[random_index][0] + [data[random_index][1]]).float()
     output_net = net.forward(input_net)
     print("The input was: {0}.\n The output was: {1}".format(input_net, output_net))
-    errors.append(criterion.forward(output_net, input_net))
+    errors.append(criterion.forward(output_net, torch.tensor(data[random_index + 1][0])))
 
 errors = torch.stack(errors)
 print("The mean of the errors is {0}".format(errors.mean()))
--- src/test_gym_q_learning_model.py
+++ src/test_gym_q_learning_model.py
@@ -1,8 +1,10 @@
 import gym
 import numpy as np
 import torch
-from representation.simple_autoencoder import Autoencoder
+from representation.autoencoder import Autoencoder
 from sklearn.utils.extmath import cartesian
+import matplotlib.pyplot as plt
+import pickle
 
 
 # env = gym.make("Taxi-v2")
@@ -37,7 +39,7 @@ def binModel(lowerBound, upperBound, numFeatures, bins=10):
     return featureBins[:, :-1]
 
 
-def getState(allStates, stateBins, features):
+def getState(stateBins, features):
     # print(len(stateBins))
     # print(len(features))
     assert len(stateBins) == len(features)
@@ -47,7 +49,7 @@ def getState(allStates, stateBins, features):
     return stateIndex
 
 
-net = Autoencoder()
+net = Autoencoder(4, 3, 4)
 net.load_state_dict(torch.load('../models/very-simple.model'))
 
 numFeatures = net.encoder.out_features
@@ -67,6 +69,7 @@ print(allStates[0, :])
 qValues = np.zeros([len(allStates), env.action_space.n])
 totalReward = 0
 alpha = 0.618
+GAMMA = 0.995
 total_episode = 10000
 print(f'qValues.shape: {qValues.shape}')
 
@@ -74,32 +77,45 @@ for episode in range(1, total_episode):
     done = False
     totalReward, reward = 0, 0
     state = env.reset()
-    explore_rate = (1 - episode / total_episode) / 10 + 0.05
+    explore_rate = (1 - episode / total_episode) / 10 + 0.01
     # here we need to find the state of all states
     # transform to encoder latent representation
     state = net.activation(net.encoder(torch.tensor(state).float())).tolist()
     # print(f'latent: {state}')
-    stateInd = getState(allStates, stateBins, state)
+    stateInd = getState(stateBins, state)
+    updateQ = []
     while not done:
-            if np.random.rand() > explore_rate:  # exploit
-                action = np.argmax(qValues[stateInd])  # 1
-            else:  # explore
-                action = np.random.randint(0, len(qValues[0]))
-            # print(f'action: {action}')
-            internalRepr, reward, done, info = env.step(action)  # 2
-            # print(reward)
-            latentRepr = net.activation(net.encoder(torch.tensor(internalRepr).float())).tolist()
-            # print(f'asdawdwdad {latentRepr}')
-            indState2 = getState(allStates, stateBins, latentRepr)  # find the state bin
-            # indState2 = np.where(allStates == state2)[0][0]
-            qValues[stateInd, action] += alpha * (reward + np.max(qValues[indState2]) - qValues[stateInd, action])  # 3
-            totalReward += reward
-            stateInd = indState2
-            # print(stateInd)
+        if np.random.rand() > explore_rate:  # exploit
+            action = np.argmax(qValues[stateInd])  # 1
+        else:  # explore
+            action = np.random.randint(0, len(qValues[0]))
+        # print(f'action: {action}')
+        internalRepr, reward, done, info = env.step(action)  # 2
+        totalReward += reward
+        latentRepr = net.activation(net.encoder(torch.tensor(internalRepr).float())).tolist()
+        indState2 = getState(stateBins, latentRepr)  # find the state bin
+        # qValues[stateInd, action] += alpha * (reward + np.max(qValues[indState2]) - qValues[stateInd, action])  # 3
+        qValues[stateInd, action] = reward + GAMMA * np.max(qValues[indState2])  # 3
+        # add state and action for later backtracking
+        updateQ.append([stateInd, action])
+        stateInd = indState2
+        if done:
+            updateQ.pop()
+        # print(stateInd)
+    # update the qValues that led to that state
+    while len(updateQ) > 0:
+        oldStateInd, oldaction = updateQ.pop()
+        qValues[oldStateInd, oldaction] = reward + GAMMA * np.max(qValues[stateInd])
+        stateInd, action = oldStateInd, oldaction
     if episode % 50 == 0:
         print('Episode {}, explore rate {}, Total Reward: {}'.format(episode, round(explore_rate, 2), totalReward))
 
 print(np.sum(qValues)/np.count_nonzero(qValues))
+
+print(np.max(qValues, axis=-1))
+
+with open('../data/qValues.pkl', 'wb') as f:
+    pickle.dump(qValues, f)
 
 done = False
 state = env.reset()
@@ -107,7 +123,7 @@ test_total_reward = 0
 while not done:
     env.render()
     state = net.activation(net.encoder(torch.tensor(state).float())).tolist()
-    stateInd = getState(allStates, stateBins, state)
+    stateInd = getState(stateBins, state)
     # stateInd = np.where(allStates == state)[0][0]
     action = np.argmax(qValues[stateInd])
     state, reward, done, _ = env.step(action)

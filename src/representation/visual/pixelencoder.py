e5a8a16ced530ef0164a998941271f96fccc0021
weidler <web@tonioweidler.de>
2018-12-08 14:42:36 +0100

    SimplePixelEncoder supposed to learn representing States with moving agent. Focus on the Agent is very difficult here, it takes long until the network stops ignoring the agents pixel.

--- src/representation/visual/pixelencoder.py
+++ src/representation/visual/pixelencoder.py
@@ -1,9 +1,9 @@
+import random
+
 import numpy as np
 import torch
-import torchvision
-import torchvision.transforms as transforms
 
-from src.task.pathing import ObstaclePathing
+from src.task.pathing import ObstaclePathing, SimplePathing
 
 
 class PixelEncoder(torch.nn.Module):
@@ -34,20 +34,58 @@ class PixelEncoder(torch.nn.Module):
         return unconved
 
 
+class SimplePixelEncoder(torch.nn.Module):
+
+    def __init__(self, width: int, height: int, repr_size: int):
+        super(SimplePixelEncoder, self).__init__()
+
+        self.encoder = torch.nn.Linear(width * height, repr_size)
+        self.decoder = torch.nn.Linear(repr_size, width * height)
+
+    def forward(self, input_tensor):
+        original_shape = input_tensor.shape
+
+        # Reshape IMAGE -> VECTOR
+        flattened = input_tensor.view(1, -1)
+
+        encoded = self.encoder(flattened)
+        decoded = self.decoder(encoded)
+
+        # Reshape VECTOR -> IMAGE
+        deflattened = decoded.reshape(original_shape)
+
+        return deflattened
+
+
 if __name__ == "__main__":
+    env = ObstaclePathing(30, 30, [[4, 9, 3, 8], [14, 19, 20, 26]])
+    env = SimplePathing(10, 10)
+    env.visualize()
 
-    task = ObstaclePathing(30, 30, [[4, 9, 3, 8], [14, 19, 20, 26]])
-    task.visualize()
 
-    img = torch.from_numpy(np.matrix(task.get_pixelbased_representation()))
-    img = img.view(1, 1, task.width, task.height).float()
+    def make_steps(n=10):
+        for step in range(n):
+            env.step(random.randint(0, 3))
 
-    net = PixelEncoder()
+
+    def get_tensor():
+        img = torch.from_numpy(np.matrix(env.get_pixelbased_representation()))
+        img = img.view(1, 1, env.width, env.height).float()
+
+        return img
+
+
+    net = SimplePixelEncoder(env.width, env.height, 50)
     criterion = torch.nn.MSELoss()
     optimizer = torch.optim.SGD(net.parameters(), lr=0.05)
-    epochs = 100000
+    epochs = 1000000    # in a 10 by 10 simple env it needs at least around 800,000 epochs to have an idea where the
+                        # agent is currently at if he is moving
 
     for epoch in range(epochs):
+        # new sample
+        make_steps(10)
+        img = get_tensor()
+
         optimizer.zero_grad()
 
         out = net(img)
@@ -58,7 +96,7 @@ if __name__ == "__main__":
 
         if epoch % (epochs / 10) == 0:
             print("{2}%; Epoch: {0}/{1}".format(epoch, epochs, round(epoch / epochs * 100, 0)))
-            task.visualize(img=out.tolist()[0][0])
+            env.visualize(img=out.tolist()[0][0])
 
     out = net(img)
-    task.visualize(img=out.tolist()[0][0])
+    env.visualize(img=out.tolist()[0][0])

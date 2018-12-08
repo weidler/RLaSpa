9dbbbb12877412daad6112565cb9a61a06ad1d9b
Danni <liu.danni@hotmail.com>
2018-12-08 23:51:51 +0100

    Add experiment logging for q_learning_model
    experiments running

--- src/learning/q_learning_model.py
+++ src/learning/q_learning_model.py
@@ -5,19 +5,15 @@ from src.representation.autoencoder import Autoencoder
 from sklearn.utils.extmath import cartesian
 import matplotlib.pyplot as plt
 from src.utils.task_dictionary import task_dict
+from src.utils.exploration import boltzmann_explore
 import pickle
-
-task_name = 'cartpole'
-model_name = 'very-simple'  # which auto-encoder to use
-
-env = gym.make(task_dict[task_name])
-env.reset()  # returns initial state
+import time
 
 
 def binThese(lowerBound, upperBound, bins=10, cutOffInf=10000):
     assert len(upperBound) == len(lowerBound)
     numFeatures = len(upperBound)
-    featureBins = np.zeros((numFeatures, bins+1))
+    featureBins = np.zeros((numFeatures, bins + 1))
     for i in range(numFeatures):
         lb = -cutOffInf if lowerBound[i] <= -cutOffInf else lowerBound[i]
         ub = cutOffInf if upperBound[i] >= cutOffInf else upperBound[i]
@@ -26,7 +22,7 @@ def binThese(lowerBound, upperBound, bins=10, cutOffInf=10000):
 
 
 def binModel(lowerBound, upperBound, numFeatures, bins=10):
-    featureBins = np.zeros((numFeatures, bins+1))
+    featureBins = np.zeros((numFeatures, bins + 1))
     for i in range(numFeatures):
         lb = lowerBound
         ub = upperBound
@@ -40,100 +36,121 @@ def getState(stateBins, features):
     assert len(stateBins) == len(features)
     bin = [np.where(stateBins[i] <= f)[0][-1] for i, f in enumerate(features)]
     # stateIndex = feature1*binSize^numRestFeatures + feature2*binSize^numRestFeatures...
-    stateIndex = int(np.sum(bin * np.power(len(stateBins[0]), np.linspace(len(features)-1, 0, len(features)))))
+    stateIndex = int(np.sum(bin * np.power(len(stateBins[0]), np.linspace(len(features) - 1, 0, len(features)))))
     return stateIndex
 
 
-# get shape of saved weights
-net_weights = torch.load('../../models/' + model_name + '.model')
-encoder_shape = list(net_weights.values())[0].size()
-decoder_shape = list(net_weights.values())[len(net_weights)-1].size()
+def run(task_names, num_iters=1, alpha=0.9, gamma=0.99, num_episodes=1000, verbose=False):
 
-# init autoencoder according to size of loaded weights
-net = Autoencoder(encoder_shape[1], encoder_shape[0], decoder_shape[0])
-net.load_state_dict(torch.load('../../models/' + model_name + '.model'))
+    for iter_idx in range(num_iters):
+
+        for task_name in task_names:
+
+            env = gym.make(task_dict[task_name])
+            env.reset()  # returns initial state
+            q_values = np.zeros([len(allStates), env.action_space.n])
+            episode_reward = np.zeros(total_episode)
+
+            for episode in range(1, num_episodes):
+                done = False
+                total_reward, reward = 0, 0
+                state = env.reset()
+                # here we need to find the state of all states
+                # transform to encoder latent representation
+                # TODO: in case of sas.model, also need to append action
+                # state = np.pad(state, (0, 2), 'constant', constant_values=(0))
+
+                # print(len(state), num_latent_features)
+                state = np.pad(state, (0, num_input_features - len(state)), mode='constant')
+                state = net.activation(net.encoder(torch.tensor(state).float())).tolist()
+
+                stateInd = getState(stateBins, state)
+                updateQ = []
+                while not done:
+                    action = boltzmann_explore(q_values[stateInd], T)
+
+                    state, reward, done, info = env.step(action)  # 2
+                    total_reward += reward
+
+                    state = np.pad(state, (0, num_input_features - len(state)), mode='constant')
+                    latentRepr = net.activation(net.encoder(torch.tensor(state).float())).tolist()
+                    indState2 = getState(stateBins, latentRepr)  # find the state bin
+
+                    # qValues[stateInd, action] += alpha * (reward + np.max(qValues[indState2]) - qValues[stateInd, action])  # 3
+                    q_values[stateInd, action] = reward + gamma * np.max(q_values[indState2])  # 3
+
+                    # add state and action for later backtracking
+                    updateQ.append([stateInd, action])
+                    stateInd = indState2
+                    if done:
+                        updateQ.pop()
+
+                # update the qValues that led to that state
+                while len(updateQ) > 0:
+                    oldStateInd, oldaction = updateQ.pop()
+                    q_values[oldStateInd, oldaction] = reward + gamma * np.max(q_values[stateInd])
+                    stateInd, action = oldStateInd, oldaction
+
+                episode_reward[episode] = total_reward
+
+                if episode % 50 == 0:
+                    print('Episode {}, Total Reward: {}'.format(episode, total_reward))
+
+            print('Saving q-values...')
+            with open('_'.join(['../../data/q_vals', task_name, model_name, str(iter_idx)]) + '.pkl', 'wb') as f:
+                pickle.dump(q_values, f)
+
+            if verbose:
+                with open('_'.join(['../../data/rewards', task_name, model_name, str(iter_idx)]) + '.pkl', 'wb') as f:
+                    pickle.dump(episode_reward, f)
+
+            print('Testing...')
+            done = False
+            state = env.reset()
+            test_total_reward = 0
+            while not done:
+                env.render()
+                time.sleep(0.01)
+
+                state = np.pad(state, (0, num_input_features - len(state)), mode='constant')
+                state = net.activation(net.encoder(torch.tensor(state).float())).tolist()
+                stateInd = getState(stateBins, state)
+
+                # stateInd = np.where(allStates == state)[0][0]
+                action = np.argmax(q_values[stateInd])
+                state, reward, done, _ = env.step(action)
+                test_total_reward += reward
+            print("Ended with reward: ", test_total_reward)
+
+            env.close()
 
 
-num_features = net.encoder.out_features
+if __name__ == '__main__':
+    task_names = ['cartpole', 'mountain_car']
+    model_name = 'very-simple-multi-task'  # which auto-encoder to use
 
-# in CartPole-v0, parameters are:
-# [position of cart, velocity of cart, angle of pole, rotation rate of pole]
-# print('low', env.observation_space.low)
-# print('high', env.observation_space.high)
-bins = 50
-# stateBins = binThese(env.observation_space.low, env.observation_space.high, bins=bins, cutOffInf=5)
-stateBins = binModel(0, 1, num_features, bins=bins)
-allStates = cartesian(stateBins)  # round
+    # get shape of saved weights
+    net_weights = torch.load('../../models/' + model_name + '.model')
+    encoder_shape = list(net_weights.values())[0].size()
+    decoder_shape = list(net_weights.values())[len(net_weights) - 1].size()
 
-# print(stateBins)
-print(allStates[0, :])
+    # init autoencoder according to size of loaded weights
+    net = Autoencoder(encoder_shape[1], encoder_shape[0], decoder_shape[0])
+    net.load_state_dict(torch.load('../../models/' + model_name + '.model'))
+    input_template = np.zeros(encoder_shape[1])
 
-qValues = np.zeros([len(allStates), env.action_space.n])
-totalReward = 0
-alpha = 0.618
-GAMMA = 0.995
-total_episode = 10000
-print(f'qValues.shape: {qValues.shape}')
+    num_input_features = net.encoder.in_features
+    num_latent_features = net.encoder.out_features
 
-for episode in range(1, total_episode):
-    done = False
-    totalReward, reward = 0, 0
-    state = env.reset()
-    explore_rate = (1 - episode / total_episode) / 10 + 0.01
-    # here we need to find the state of all states
-    # transform to encoder latent representation
-    # TODO: in case of sas.model, also need to append action
-    state = net.activation(net.encoder(torch.tensor(state).float())).tolist()
-    # print(f'latent: {state}')
-    stateInd = getState(stateBins, state)
-    updateQ = []
-    while not done:
-        if np.random.rand() > explore_rate:  # exploit
-            action = np.argmax(qValues[stateInd])  # 1
-        else:  # explore
-            action = np.random.randint(0, len(qValues[0]))
-        # print(f'action: {action}')
-        internalRepr, reward, done, info = env.step(action)  # 2
-        totalReward += reward
-        latentRepr = net.activation(net.encoder(torch.tensor(internalRepr).float())).tolist()
-        indState2 = getState(stateBins, latentRepr)  # find the state bin
-        # qValues[stateInd, action] += alpha * (reward + np.max(qValues[indState2]) - qValues[stateInd, action])  # 3
-        qValues[stateInd, action] = reward + GAMMA * np.max(qValues[indState2])  # 3
-        # add state and action for later backtracking
-        updateQ.append([stateInd, action])
-        stateInd = indState2
-        if done:
-            updateQ.pop()
-        # print(stateInd)
-    # update the qValues that led to that state
-    while len(updateQ) > 0:
-        oldStateInd, oldaction = updateQ.pop()
-        qValues[oldStateInd, oldaction] = reward + GAMMA * np.max(qValues[stateInd])
-        stateInd, action = oldStateInd, oldaction
-    if episode % 50 == 0:
-        print('Episode {}, explore rate {}, Total Reward: {}'.format(episode, round(explore_rate, 2), totalReward))
+    num_bins = 100
 
-print(np.sum(qValues)/np.count_nonzero(qValues))
+    stateBins = binModel(0, 1, num_latent_features, bins=num_bins)
+    allStates = cartesian(stateBins)  # round
 
-print(np.max(qValues, axis=-1))
+    # totalReward = 0
+    alpha = 0.618
+    gamma = 0.995
+    T = 3
+    total_episode = 100000
 
-print('Saving q-values...')
-with open('_'.join(['../../data/q_vals', task_name, model_name]) + '.pkl', 'wb') as f:
-    pickle.dump(qValues, f)
-
-
-print('Testing...')
-done = False
-state = env.reset()
-test_total_reward = 0
-while not done:
-    env.render()
-    state = net.activation(net.encoder(torch.tensor(state).float())).tolist()
-    stateInd = getState(stateBins, state)
-    # stateInd = np.where(allStates == state)[0][0]
-    action = np.argmax(qValues[stateInd])
-    state, reward, done, _ = env.step(action)
-    test_total_reward += reward
-print("Ended with reward: ", test_total_reward)
-
-env.close()
+    run(task_names, 10, alpha, gamma, total_episode, verbose=True)

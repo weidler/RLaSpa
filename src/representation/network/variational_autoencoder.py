7755674d731a408a4f6724b999fa4ef891d45b13
Alessandro Scoppio <alessandroscoppio@gmail.com>
2019-01-04 17:51:44 +0100

    draft of variational autoencoder network

---
+++ src/representation/network/variational_autoencoder.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+
+
+class VariationalAutoencoderNetwork(nn.Module):
+
+    def __init__(self, inputNeurons=4, hiddenNeurons=3, outputNeurons=4):
+        super(VariationalAutoencoderNetwork, self).__init__()
+
+        self.encoderMean = nn.Linear(inputNeurons, hiddenNeurons)
+        self.encoderStDev = nn.Linear(inputNeurons, hiddenNeurons)
+        self.decoder = nn.Linear(hiddenNeurons, outputNeurons)
+
+        self.activation = torch.relu
+
+    def encode(self, x):
+
+        return self.encoderMean(x), self.encoderStDev(x)
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)
+
+    def forward(self, vinput):
+        out = self.activation(self.encoder(vinput))
+        out = (self.decoder(out))
+
+        return out

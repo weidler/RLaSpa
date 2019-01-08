a91858e9d82c947cf0aaac60ae56eb75f6f33734
weidler <web@tonioweidler.de>
2019-01-08 20:00:28 +0100

    Parallel Agent integration.

---
+++ src/agents/parallel.py
@@ -0,0 +1,87 @@
+import gym
+
+from src.policy.ddqn import DoubleDeepQNetwork
+from src.policy.policy import _Policy
+from src.representation.learners import SimpleAutoencoder
+from src.representation.representation import _RepresentationLearner
+
+
+class ParallelAgent:
+    policy: _Policy
+    representation_learner: _RepresentationLearner
+
+    def __init__(self, representation_learner: _RepresentationLearner, policy: _Policy, environment):
+        self.representation_learner = representation_learner
+        self.policy = policy
+        self.env = environment
+
+    # REINFORCEMENT LEARNING #
+
+    def train_agent(self, episodes: int, max_episode_length=1000):
+        print("Starting parallel training process.")
+        rewards = []
+        for episode in range(episodes):
+            done = False
+            current_state = env.reset()
+            latent_state = self.representation_learner.encode(current_state)
+            episode_reward = 0
+            steps = 0
+            while not done and steps < max_episode_length:
+                # choose action
+                action = self.policy.choose_action(latent_state, episode)
+
+                # step and observe
+                observation, reward, done, _ = self.env.step(action)
+                latent_observation = self.representation_learner.encode(observation)
+
+                # train the REPRESENTATION learner
+                self.representation_learner.learn(state=current_state, next_state=observation, reward=reward,
+                                                  action=action)
+
+                # train the POLICY
+                self.policy.update(latent_state, action, reward, latent_observation, done)
+
+                # update states (both, to avoid redundant encoding)
+                current_state = observation
+                latent_state = latent_observation
+
+                # trackers
+                episode_reward += reward
+                steps += 1
+
+            rewards.append(episode_reward)
+
+            if episode % (episodes // 20) == 0: print(
+                f"\t|-- {round(episode/episodes * 100)}% (Avg. Rew. of {sum(rewards[-(episodes//20):])/(episodes//20)})")
+
+        # Last update of the agent policy
+        self.policy.finish_training()
+
+    # TESTING #
+
+    def act(self, current_state):
+        current_state = self.representation_learner.encode(current_state)
+        action = self.policy.choose_action_policy(current_state)
+        next_state, step_reward, env_done, _ = self.env.step(action)
+        return next_state, env_done
+
+
+if __name__ == "__main__":
+    env = gym.make("CartPole-v0")
+    repr_learner = SimpleAutoencoder(4, 2, 3)
+    policy = DoubleDeepQNetwork(3, 2)
+
+    # AGENT
+    agent = ParallelAgent(repr_learner, policy, env)
+
+    # TRAIN
+    agent.train_agent(100)
+
+    # TEST
+    done = False
+    state = env.reset()
+    step = 0
+    while not done and step < 1000:
+        env.render()
+        state, done = agent.act(state)
+        step += 1

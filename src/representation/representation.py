4c1b6e080fec6c1c9a1c5af6617ac5b1e3b6c5b3
weidler <web@tonioweidler.de>
2019-01-11 16:08:02 +0100

    fully annotated and documented the RepresentationLearner acstract class

--- src/representation/representation.py
+++ src/representation/representation.py
@@ -1,5 +1,4 @@
 import abc
-import random
 import torch
 from torch import Tensor
 from typing import List
@@ -8,34 +7,35 @@ from src.utils.container import SARSTuple
 
 
 class _RepresentationLearner(abc.ABC):
+    """ Abstract representation module class. A representation module is capable of learning to represent a given state
+    in the latent space.
+    """
 
     @abc.abstractmethod
     def encode(self, state: Tensor) -> Tensor:
+        """ Encode the given state tensor into a latent representation.
+
+        :param state:   the state to be encoded, given as a tensor
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
-    def learn(self, state: Tensor, action: Tensor, reward: Tensor, next_state: Tensor, remember: bool = True) -> float:
+    def learn(self, state: Tensor, action: Tensor, reward: Tensor, next_state: Tensor) -> float:
+        """ Learn from a presented (batch of) example(s). Learning examples are SARS tuples. Although not all
+        implementations will need all four components, for consistency they need to be capable of getting passed each.
+
+        :param state:       the current state of the environment when the action was taken
+        :param action:      the action taken in the environment
+        :param reward:      the reward received for taking the action in the state
+        :param next_state:  the state observed after taking the action
+        """
         raise NotImplementedError
 
-    def learn_batch_of_tuples(self, batch: List[SARSTuple]):
-        # batch_size = len(batch)
-        # state_size = batch[0].state.shape
-        # action_size = batch[0].action.shape
-
+    def learn_batch_of_tuples(self, batch: List[SARSTuple]) -> None:
         state_batch = torch.stack([s.state for s in batch], 0)
         action_batch = torch.stack([s.action for s in batch], 0)
         reward_batch = torch.Tensor([s.reward for s in batch])
         next_state_batch = torch.stack([s.next_state for s in batch], 0)
-
-        # state_batch = torch.zeros((batch_size,) + state_size)
-        # action_batch = torch.zeros((batch_size,) + action_size)
-        # reward_batch = torch.zeros((batch_size, 1))
-        # next_state_batch = torch.zeros((batch_size,) + state_size)
-        # for i, sars_tuple in enumerate(batch):
-        #     state_batch.append(sars_tuple.state)
-        #     action_batch.append(sars_tuple.action)
-        #     reward_batch.append(sars_tuple.reward)
-        #     next_state_batch.append(sars_tuple.next_state)
 
         # learn
         self.learn(

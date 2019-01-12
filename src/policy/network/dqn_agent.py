fbac8b7171c33004cfc7a8ac8c122ee4d6af73df
Adrián Rodríguez Grillo <adrirgrillo@gmail.com>
2019-01-12 17:44:33 +0100

    Added full backpropagation to the dueling ddqn and prioritized dqn

--- src/policy/network/dqn_agent.py
+++ src/policy/network/dqn_agent.py
@@ -5,7 +5,15 @@ import torch.nn as nn
 
 
 class DQN(nn.Module):
-    def __init__(self, num_features, num_actions, representation_network):
+    def __init__(self, num_features: int, num_actions: int, representation_network: torch.nn.Module):
+        """
+        Initializes the neural network that will predict the q value of the states
+
+        :param num_features: number of features in the state
+        :param num_actions: number of actions that the agent can do
+        :param representation_network: Optional nn.Module used for the representation. Including it into the policy
+        network allows full backpropagation.
+        """
         super(DQN, self).__init__()
         self.num_features = num_features
         self.num_actions = num_actions

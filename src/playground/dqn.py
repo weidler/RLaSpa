3d9d40a90e9fd2d2783447755d86ce7bc1a1c419
Adrián Rodríguez Grillo <adrirgrillo@gmail.com>
2019-01-15 18:37:32 +0100

    File for testing

--- src/playground/dqn.py
+++ src/playground/dqn.py
@@ -1,9 +1,10 @@
-import argparse
+import time
 
 import gym
 import torch
 
-from src.policy.dqn_prioritized import PrioritizedDuelingDeepQNetwork
+from src.policy.dqn import DoubleDeepQNetwork
+from src.utils.logger import Logger
 from src.utils.schedules import ExponentialSchedule, LinearSchedule
 
 
@@ -13,27 +14,35 @@ def train(iterations: int):
     all_rewards = []
     episode_reward = 0
     episode_loss = 0
+    episodes = 0
     for iteration in range(1, iterations + 1):
         action = model.choose_action(state=state)
 
         next_state, reward, done, _ = env.step(action)
         next_state = torch.tensor(next_state).float()
 
-        episode_loss += model.update(state, action, reward, next_state, done)
+        loss = model.update(state, action, reward, next_state, done)
+
+        episode_loss += loss
 
         state = next_state
         episode_reward += reward
 
         if done:
+            info = {'loss': episode_loss, 'reward': episode_reward}
+            logger.scalar_summary_dict(info, iteration)
             torch.tensor(env.reset()).float()
             all_rewards.append(episode_reward)
             losses.append(episode_loss)
             episode_reward = 0
             episode_loss = 0
+            episodes += 1
 
         if iteration % 200 == 0:
             print('Iteration: {0}'.format(iteration))
             print('Rewards: {0}'.format(all_rewards[-9:]))
+    model.finish_training()
+    print("Played {0} episodes".format(episodes))
 
 
 def play(iterations: int, render=True):
@@ -58,29 +67,22 @@ def play(iterations: int, render=True):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='DQN agent execution')
-    parser.add_argument('--env', type=str, metavar='E', default='CartPole-v0', help='GYM environment')
-    parser.add_argument('--init_eps', type=float, metavar='I', default=1.0, help='Initial epsilon')
-    parser.add_argument('--min_eps', type=float, metavar='M', default=0.01, help='Minimum epsilon')
-    parser.add_argument('--eps_decay', type=int, metavar='D', default=500, help='Epsilon decay')
-    parser.add_argument('--gamma', type=int, metavar='G', default=0.99, help='Gamma')
-    parser.add_argument('--memory_size', type=int, metavar='S', default=10000, help='Memory size')
-    parser.add_argument('--batch_size', type=int, metavar='B', default=32, help='Batch size')
-    parser.add_argument('--iterations', type=int, metavar='IT', default=30000, help='Training iterations')
-    args = parser.parse_args()
-
-    env = gym.make(args.env)
+    env = gym.make('CartPole-v0')
     number_of_observations = env.observation_space.shape[0]
     number_of_actions = env.action_space.n
+
     memory_delay = 0
     init_eps = 1.0
     memory_eps = 0.8
-    min_eps = 0.01
+    min_eps = 0.001
     eps_decay = 10000
     linear = LinearSchedule(schedule_timesteps=memory_delay, initial_p=init_eps, final_p=memory_eps)
     exponential = ExponentialSchedule(initial_p=memory_eps, min_p=min_eps, decay=eps_decay)
-    model = PrioritizedDuelingDeepQNetwork(num_features=number_of_observations, num_actions=number_of_actions,
-                                           eps_calculator=linear, memory_eps_calculator=exponential,
-                                           memory_delay=memory_delay)
-    train(iterations=args.iterations)
+    model = DoubleDeepQNetwork(num_features=number_of_observations, num_actions=number_of_actions,
+                               eps_calculator=linear, memory_eps_calculator=exponential,
+                               memory_delay=memory_delay)
+
+    logger = Logger('logs', 'dqn_playground_{0}'.format(time.time()))
+
+    train(iterations=50000)
     play(iterations=10000, render=True)

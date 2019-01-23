f5bc59ac9cbefef4e8ee1b151e06ca9863de6bd8
Kevin Trebing <Kevin.Trebing@gmx.net>
2019-01-23 13:00:51 +0100

    plotting of rewards

--- src/learning/final/plot_from_file.py
+++ src/learning/final/plot_from_file.py
@@ -1,14 +1,34 @@
 import matplotlib.pyplot as plt
-import csv
+import numpy as np
+from scipy.signal import savgol_filter
+
 
 episode = []
 avg = []
-with open('example.txt','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=' ')
-    for row in plots:
-        episode.append(int(row[2]))
-        avg.append(float(row[12]))
-plt.plot(episode,avg, label='Reward over episode')
+with open('JanusPixel_Race_1m_tunnel.txt','r') as f:
+    data = f.readlines()
+    for row in data:
+        split_row = row.split(" ")
+        episode.append(int(split_row[2]))
+        avg.append(float(split_row[12]))
+episode_tunnel = np.array(episode) - 1000000
+avg_tunnel = np.array(avg)
+
+episode = []
+avg = []
+with open('JanusPixel_Race_735k_scroll.txt','r') as f:
+    data = f.readlines()
+    for row in data:
+        split_row = row.split(" ")
+        episode.append(int(split_row[2]))
+        avg.append(float(split_row[12]))
+episode_scrollers = np.array(episode) - 735000
+avg_scrollers = np.array(avg)
+
+
+intervals = np.arange(0,100,5)*500
+plt.plot(episode_tunnel, savgol_filter(avg_tunnel, 13, 1), label='Trained on tunnel')
+plt.plot(episode_scrollers, savgol_filter(avg_scrollers, 13, 1), label='Trained on scrollers')
 plt.xlabel('episodes')
 plt.ylabel('reward')
 plt.title('Reward over episodes')
